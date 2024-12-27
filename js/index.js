a157cddf541554b92cb0d34c95f669563d393482
Agustin Farias <sstadix@gmail.com>
2024-12-27 10:52:42 -0300

    Finalizacion del diseño e inicio de mediaqueries

--- js/index.js
+++ js/index.js
@@ -1,30 +1,30 @@
-// Progress Bar Animation
-document.addEventListener('DOMContentLoaded', function () {
-    const progressBars = document.querySelectorAll('.progress-bar');
-    let animated = false; // Para evitar que se anime varias veces
+// // Progress Bar Animation
+// document.addEventListener('DOMContentLoaded', function () {
+//     const progressBars = document.querySelectorAll('.progress-bar');
+//     let animated = false; // Para evitar que se anime varias veces
 
-    function animateBars() {
-        progressBars.forEach(bar => {
-            const percentage = bar.getAttribute('data-percentage');
-            const progressElement = bar.querySelector('.progress');
-            progressElement.style.width = percentage + '%'; // Establece el ancho de la barra de progreso
-        });
-    }
+//     function animateBars() {
+//         progressBars.forEach(bar => {
+//             const percentage = bar.getAttribute('data-percentage');
+//             const progressElement = bar.querySelector('.progress');
+//             progressElement.style.width = percentage + '%'; // Establece el ancho de la barra de progreso
+//         });
+//     }
 
-    function checkScroll() {
-        const abilitiesSection = document.querySelector('.habilities');
-        const sectionPosition = abilitiesSection.getBoundingClientRect().top;
-        const screenPosition = window.innerHeight;
+//     function checkScroll() {
+//         const abilitiesSection = document.querySelector('.portfolio');
+//         const sectionPosition = abilitiesSection.getBoundingClientRect().top;
+//         const screenPosition = window.innerHeight;
 
-        // Verifica si la sección está visible en la ventana
-        if (!animated && sectionPosition < screenPosition) {
-            animateBars();
-            animated = true; // Evita que se anime nuevamente
-        }
-    }
+//         // Verifica si la sección está visible en la ventana
+//         if (!animated && sectionPosition < screenPosition) {
+//             animateBars();
+//             animated = true; // Evita que se anime nuevamente
+//         }
+//     }
 
-    window.addEventListener('scroll', checkScroll);
-});
+//     window.addEventListener('scroll', checkScroll);
+// });
 
 // -----------------------------------------------------
 document.addEventListener('DOMContentLoaded', function() {
@@ -64,12 +64,12 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
         // Mostrar/Ocultar el botón de volver arriba
-        const scrollToTopBtn = document.getElementById('scrollToTopBtn');
-        if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-            scrollToTopBtn.style.display = "block"; // Muestra el botón
-        } else {
-            scrollToTopBtn.style.display = "none"; // Oculta el botón
-        }
+        // const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+        // if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        //     scrollToTopBtn.style.display = "block"; // Muestra el botón
+        // } else {
+        //     scrollToTopBtn.style.display = "none"; // Oculta el botón
+        // }
     }
 
     window.addEventListener('scroll', checkScroll);
